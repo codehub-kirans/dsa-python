e827826e0184931dd3365216d18fd03f6bfcb0d5
Kiran S <codehub.kirans@gmail.com>
2022-01-14 20:27:49 +0530

    Implemention for autocorrect

--- autoComplete.py
+++ autoComplete.py
@@ -1,8 +1,28 @@
-"""Autocomplete using Tries"""
+##################################################################
+##
+##                       DOCUMENTATION
+##
+##################################################################
+#
+#
+# @file	autoComplete.py
+# @version	1
+# @author	Kiran S
+# @short	Word Autocomplete & autocorrect
+# Autocomplete using Tries
+#
+# <p>
+# See GIT for detailed history.
+# <p>
+# Copyright (c) 2021 Kiran S. All rights reserved.
+#
+##################################################################
+
 
 class TrieNode:
     def __init__(self) -> None:
         self.children = {}
+
 
 class Trie:
     def __init__(self) -> None:
@@ -19,7 +39,34 @@ class Trie:
                 return None
 
         return currentNode
-    
+
+    def longestMatch(self, word):
+        currentNode = self.root
+        longestMatch = ""
+
+        for char in word:
+            childNode = currentNode.children.get(char)
+            if childNode:
+                currentNode = childNode
+                longestMatch += char
+            else:
+                break
+
+        return longestMatch, currentNode
+
+    def keyTraversal(self, node=None, keyString=[]):
+        currentNode = node or self.root
+        keys = ""
+        if currentNode:
+            for childkey, childnode in currentNode.children.items():
+                keys = keys + childkey + ","
+            keyString.append(keys[:-1])
+
+            for childkey, childnode in currentNode.children.items():
+                if childkey != "*":
+                    self.keyTraversal(childnode, keyString)
+        return keyString
+
     def insert(self, word):
         currentNode = self.root
 
@@ -33,27 +80,40 @@ class Trie:
                 currentNode.children[char] = childNode
                 currentNode = childNode
 
-        currentNode.children['*'] = None
+        currentNode.children["*"] = None
 
-    def collectWords(self, node = None, word = "", words = []):
+    def collectWords(self, node=None, word="", words=[]):
         currentNode = node or self.root
 
         for key, childNode in currentNode.children.items():
             if key == "*":
-                words = words.append(word)
+                words.append(word)
             else:
-                self.collectWords(childNode, word+key, words)
-        
+                self.collectWords(childNode, word + key, words)
+
         return words
 
     def autoCompleteWord(self, prefix):
         searchNode = self.search(prefix)
         if searchNode:
-            return self.collectWords(searchNode, prefix, [])
+            return self.collectWords(searchNode)
         else:
             return None
 
-#Test
+    def autoCorrectWord(self, prefix):
+        searchNode = self.search(prefix)
+        words = []
+
+        if searchNode:
+            return prefix
+        else:
+            longestMatch, startNode = self.longestMatch(prefix)
+            self.collectWords(startNode, longestMatch, words)
+
+        return words
+
+
+# Test
 autoComplete = Trie()
 autoComplete.insert("cat")
 autoComplete.insert("man")
@@ -62,7 +122,7 @@ autoComplete.insert("can")
 autoComplete.insert("call")
 print("Autocomplete for ca: ", autoComplete.autoCompleteWord("ca"))
 print("Autocomplete for ma: ", autoComplete.autoCompleteWord("ma"))
-print("Autocomplete for z: ",autoComplete.autoCompleteWord("z"))
-        
-        
-
+print("Autocomplete for z: ", autoComplete.autoCompleteWord("z"))
+print("Keys: ", autoComplete.keyTraversal())
+print("Autocorrect for mat: ", autoComplete.autoCorrectWord("mat"))
+print("Autocorrect for cad: ", autoComplete.autoCorrectWord("cad"))
